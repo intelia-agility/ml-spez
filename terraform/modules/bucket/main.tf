c6efc161f510880f1f11321351e478ddcd018e0b
Varun <109652048+varun-intelia@users.noreply.github.com>
2023-10-19 16:33:02 +1100

    edited bucket name suffix

--- terraform/modules/bucket/main.tf
+++ terraform/modules/bucket/main.tf
@@ -1,7 +1,7 @@
 # Create a Google Cloud Storage (GCS) bucket resource.
 resource "google_storage_bucket" "bucket" {
   # Set the name of the GCS bucket, combining the bucket name and project ID.
-  name     = "${var.bucket_name}_${var.project_id}"
+  name     = "${var.bucket_name}-${var.project_id}"
 
   # Specify the location for the GCS bucket, provided as an input variable.
   location = var.location
