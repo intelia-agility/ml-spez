426d796168fecb8548ef5b000a3ef2847d0fe43d
Varun <109652048+varun-intelia@users.noreply.github.com>
2024-01-19 01:27:05 +1100

    removed rows without predictions

--- cloud_functions/trans/main.py
+++ cloud_functions/trans/main.py
@@ -197,7 +197,7 @@ def get_weighted_embeddings() -> bool:
     FROM
         `ml-spez-ccai.processed.embeddings-2024`
     WHERE
-        content != ""
+        content != "" AND predictions IS NOT null
     GROUP BY
         job_id;
     '''
