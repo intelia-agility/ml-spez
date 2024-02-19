cb405a02a2d8fa7c8fd1369f6b96bccbc4b8ab95
Varun <109652048+varun-intelia@users.noreply.github.com>
2024-02-19 11:49:14 +1100

    Update something.js

--- definitions/something.js
+++ definitions/something.js
@@ -1,10 +1,10 @@
-const table_names = ["t1", "t2", "t3"]
+const table_names = ["t1", "t2", "t3"];
 table_names.forEach(table_name=>{
     publish(table_name, {
         type: "table",
         description: `${table_name} table is a dynamically generated table`
     })
     .query(ctx=>{
-        `SELECT * FROM ${ctx.ref(second_view)}`
+        `SELECT * FROM ${ctx.ref("second_view")}`
     });
 });
